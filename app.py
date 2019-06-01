1311ea004e784497456fc537d38f75c4ebd5bec9
jlw47 <jlw47@cms-r1-11.cms.waikato.ac.nz>
2019-06-01 17:45:30 +1200

    Initial commit, with some functionality of app.py working

---
+++ app.py
@@ -0,0 +1,75 @@
+import time
+
+import redis
+from flask import Flask
+
+from math import sqrt
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+# Determines if n is a prime number
+# Returns: boolean TTrue or False
+def is_prime(n):
+    # 2 is obv prime
+    if n == 2:
+        # Store value in redis key 'primes'
+        cache.append("primes", n)
+        return True
+    # if dividing by 2 = 0 or less than or equal to 1 => not prime
+    if n % 2 == 0 or n <= 1:
+        return False
+
+    # Find sqrt and then add 1 (only need to check up to sqrt of number)
+    sqr = int(sqrt(n)) + 1
+
+    # check all values between 3 and sqr, in intervals of 2
+    # this is efficient in terms of memory as range always uses the same small amount of memory
+    # as it only stores start, stop, step values
+    # https://docs.python.org/3/library/stdtypes.html#typesseq-range
+    for div in range(3, sqr, 2):
+        if n % div == 0: # if divisible by a divisor in range, not prime
+            return False
+    # Store value in redis key 'primes'
+    cache.append("primes", n + ",")
+    return True
+
+# Gets all prime values from the redis microservice
+def get_primes_redis():
+    x = cache.get("primes")
+    listPrimes = x.split(',')
+    del listPrimes[-1] # delete last item in list because of the way values are appended
+    
+
+# Basic route
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+# Route for Requirement 1
+# int:number uses a converter to specify the type
+@app.route('/isPrime/<int:number>')
+def isPrime(number):
+    res = is_prime(number)
+    if(res):
+        return '{} is prime'
+    return '{} is not prime'
+
+# Route for Requirement 2
+@app.route('/primesStored')
+def primesStored():
+    return 'null'
